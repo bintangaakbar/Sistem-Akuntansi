37cc4ae41dfe952693488567b490af79cb4ad4b3
bintangaakbar <Banugrah2@gmail.com>
2022-03-31 22:04:07 +0700

    Plan 1 with Template Option

---
+++ footer.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <footer id="footer">
+        <div class="container">
+            <div class="cols">
+                <div class="col">
+                    <h3>Frequently Asked Questions</h3>
+                    <ul>
+                        <li><a href="#">Fusce eget dolor adipiscing </a></li>
+                        <li><a href="#">Posuere nisl eu venenatis gravida</a></li>
+                        <li><a href="#">Morbi dictum ligula mattis</a></li>
+                        <li><a href="#">Etiam diam vel dolor luctus dapibus</a></li>
+                        <li><a href="#">Vestibulum ultrices magna </a></li>
+                    </ul>
+                </div>
+                <div class="col contact">
+                    <h3>Contact us</h3>
+                    <p>Diana’s Jewelry INC.<br>54233 Avenue Street<br>New York</p>
+                    <p><span class="ico ico-em"></span><a href="#">contact@dianasjewelry.com</a></p>
+                    <p><span class="ico ico-ph"></span>(590) 423 446 924</p>
+                </div>
+                <div class="col newsletter">
+                    <h3>Join our newsletter</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque laudantium.</p>
+                    <form action="#">
+                        <input type="text" placeholder="Your email address...">
+                        <button type="submit"></button>
+                    </form>
+                </div>
+            </div>
+            <p class="copy">Copyright 2013 Jewelry. All rights reserved.</p>
+        </div>
+        <!-- / container -->
+    </footer>
+    <!-- / footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
