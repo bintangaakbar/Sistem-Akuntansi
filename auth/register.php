6107691a16970ac5e78c12443984c4ab27ececc0
bintangaakbar <Banugrah2@gmail.com>
2022-04-07 17:59:19 +0700

    Front end half done!

---
+++ auth/register.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-dark w-auto" style="background-color: #180A0A;">
+        <div class="container">
+            <a class="navbar-brand" href="../index.php"><b>Duo Putri</b> </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex align-items-center" id="navbarNavAltMarkup">
+                <div class="navbar-nav" style="margin-left: 60rem;">
+                    <a class="nav-link" href="register.php">Register</a>
+                    <a class="nav-link" href="login.php">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</head>
+
+<body>
+    <style>
+        .csscard {
+            min-height: 200px;
+            width: 35rem;
+        }
+
+        .inputcss {
+            width: 80%;
+            margin: auto;
+            display: block;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
+    <!-- FORM -->
+    <div class="align-items-center d-flex flex-column" style="margin-top: 4rem;">
+        <div class="card csscard">
+            <form method="POST" action="">
+                <h3 style="margin-top: 1rem; text-align:center">Register</h3>
+                <div class="d-flex justify-content-center align-items-center">
+                    <hr style="width: 80%;">
+                </div>
+
+                <div class="form-group">
+                    <label for="inputEmail" style="padding-left: 57px;">Nama</label>
+                    <input type="text" class="form-control inputcss" name="nama" placeholder="Masukkan Nama Lengkap" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="inputEmail" style="padding-left: 57px;">Email</label>
+                    <input type="email" class="form-control inputcss" name="email" placeholder="Masukkan Alamat E-Mail" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="inputEmail" style="padding-left: 57px;">Nomor Handphone</label>
+                    <input type="number" class="form-control inputcss" name="no_hp" placeholder="Masukkan Nomor Handphone" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="inputPassword" style="padding-left: 57px;">Kata Sandi</label>
+                    <input type="password" class="form-control inputcss" name="password" placeholder="Kata Sandi" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="inputPassword" style="padding-left: 57px;">Konfirmasi Kata Sandi</label>
+                    <input type="password" class="form-control inputcss" name="passwordconfirm" placeholder="Konfirmasi Kata Sandi" required>
+                </div>
+
+                <!-- BUTTON -->
+                <div style="margin-top: 1rem; text-align:center">
+                    <button type="submit" class="btn btn-primary" name="register">Daftar</button>
+                </div>
+
+                <!-- PARAGRAPH -->
+                <p style="margin-top: 1rem; text-align:center"> Anda sudah punya akun? <a href="Bintang_login.php"> Login</a> </p>
+
+            </form>
+        </div>
+    </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ footer.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <footer class="footer-59391" style="position: absolute; bottom: 0; width: 100%;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="site-logo">
+                        <a href="index.php">Duo Putri</a>
+                    </div>
+                </div>
+                <div class="col-md-8 text-md-right">
+                    <ul class="list-unstyled social-icons">
+                        <a href="https://www.instagram.com/" class="instagram" target="_blank"><i class="bi bi-instagram"> </i><img src="./img/instagram-logo.png" class="logo1"> </i></a>
+                        <a href="https://www.twitter.com/" class="instagram" target="_blank"><i class="bi bi-instagram"> </i><img src="./img/icons8-twitter-48.png" class="logo1"> </i></a>
+                        <a href="https://www.facebook.com/" class="instagram" target="_blank"><i class="bi bi-instagram"> </i><img src="./img/icons8-facebook-48.png" class="logo1"> </i></a>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 ">
+                    <ul class="nav-links list-unstyled nav-left">
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Policy</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 text-md-right">
+                    <ul class="nav-links list-unstyled nav-right">
+                        <li><a href="index.php">Beranda</a></li>
+                        <li><a href="#">Produk</a></li>
+                        <li><a href="./contact/index.html">Kontak</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col ">
+                    <div class="copyright">
+                        <p>&#169; Copyright 2022. All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+
+
+
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
